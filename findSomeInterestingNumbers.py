4b224f2d79edb7b35828211c05228173a777a341
theodore <xide.theodore.liu@gmail.com>
2025-11-05 20:27:17 +0800

    234

---
+++ findSomeInterestingNumbers.py
@@ -0,0 +1,11 @@
+# def inquire(text):
+
+def main():
+    running = True
+
+    while running == True:
+        userInput = input('1:\n2:\n> ')
+
+if __name__ == "__main__":
+    print('A TheodoreProductions™ product')
+    main()
