550c58cf53d910c52eb5f23b2f46c33ddbb10ee6
theodore <xide.theodore.liu@gmail.com>
2025-11-30 15:38:36 +0800

    removed space from all but the first line in the debug text and added b.

--- decode/decodeText.py
+++ decode/decodeText.py
@@ -173,7 +173,7 @@ def run(textlist, p):
             rectlist.append([x, y, p, 7 * p, c])
             rectlist.append([x, y + 2 * p, 3 * p, p, c])
             rectlist.append([x + 3 * p, y + 3 * p, p, 3 * p, c])
-            rectlist.append([x y + 6 * p, 3 * p, p, c])
+            rectlist.append([x, y + 6 * p, 3 * p, p, c])
         elif text[0] == '0':
             rectlist.append([x + p, y, 3 * p, p, c])
             rectlist.append([x + p, y + 6 * p, 3 * p, p, c])
--- getData/worldItems.py
+++ getData/worldItems.py
@@ -13,6 +13,6 @@ def barriers(screen):
 def text(screen):
     # Text, x, y, color, size
     if screen == 'debug':
-        return [['''A BCDEFGHIJKLMNOPQRSTUVWXYZ\na bcdefghijklmnopqrstuvwxyz\n0 123456789\n. ,!?()[]{-}_=+@#$%^&*/\\|<>:;'"\n�''', 1, 1, (0, 0, 0), 1]]
+        return [['''A BCDEFGHIJKLMNOPQRSTUVWXYZ\nabcdefghijklmnopqrstuvwxyz\n0123456789\n.,!?()[]{-}_=+@#$%^&*/\\|<>:;'"\n�''', 1, 1, (0, 0, 0), 1]]
     else:
         return [['Error 0000: screen number wrong', 1, 1, (255, 0, 255), 1]]
