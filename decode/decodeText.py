2f466cde3bc46adc11817745c81886af150e1528
theodore <xide.theodore.liu@gmail.com>
2025-12-04 20:46:42 +0800

    added c, d, e, f, g, h, i, j, k, l, m and n and added text sentences after �. Also note bug there is a chance for the A to start a few pees behind where it should be. Problem is tempererily fixed by adding \n to the end of text but still needs fixing.

--- decode/decodeText.py
+++ decode/decodeText.py
@@ -232,6 +232,11 @@ def run(textlist, p):
             rectlist.append([x, y + 3 * p, p, 4 * p, c])
             rectlist.append([x + p, y + 2 * p, 2 * p, p, c])
             rectlist.append([x + 3 * p, y + 3 * p, p, 4 * p, c])
+        elif text[0] == 'o':
+            rectlist.append([x + p, y + 2 * p, 2 * p, p, c])
+            rectlist.append([x, y + 3 * p, p, 3 * p, c])
+            rectlist.append([x + 3 * p, y + 3 * p, p, 3 * p, c])
+            rectlist.append([x + p, y + 6 * p, 2 * p, p, c])
         elif text[0] == '0':
             rectlist.append([x + p, y, 3 * p, p, c])
             rectlist.append([x + p, y + 6 * p, 3 * p, p, c])
